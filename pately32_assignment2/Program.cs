4e0bd5d8b7d58c2b8cba0ccc393ac55c18619951
HP <HP@DESKTOP-E7H3AEU>
2024-03-20 10:50:17 +0530

    Add project files.

---
+++ pately32_assignment2/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine($"{i} is an even");
+                evenSum += i;
+            }
+            else
+            {
+                Console.WriteLine($"{i} is an odd");
+                oddSum += i;
+            }
+        }
+
+        Console.WriteLine($"Total of even numbers: {evenSum}");
+        Console.WriteLine($"Total of odd numbers: {oddSum}");
+    }
+}
